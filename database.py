0703c690c3db1afc3752741f336d4e14238d876e
Kristian Gunnleiv I Gardastovu Sørensen <kristiangunnleiv@gmail.com>
2025-07-16 22:12:34 +0200

    Added search_query function to database.py, as well as not opening the csv file in __init__, but rather have temporary, single instances in the functions that need to open the file. Rather the db_source is set in the init, and is used later on.

--- database.py
+++ database.py
@@ -3,15 +3,16 @@ import csv
 #select db file
 class Database:
     def __init__(self, db_source, gfx_error_handle):
-        self.item = ("", 0)
-        try:
-            self.inv = open(db_source, newline=" ")
-        except:
-            gfx_error_handle()
+        self.item_dict = dict()
+        self.db_source = db_source
+        
     
     def add_item(self, item_name, count):
         open_db = csv.writer(self.inv)
         open_db.writerow(item_name, count)
     
-    def get_item(self, item_name):
-        return item_name
+    def search_query(self, item_name):
+        raw_data = csv.DictReader(self.inv, dialect="excel")
+    
+    def edit_item(self):
+        pass
